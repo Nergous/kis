dc8349fe2277368a8b3ed714962297b6775af5ea
Attomex <e.primorov2@gmail.com>
2025-02-21 22:35:31 +0300

    Настроенный axios + аутентификацию. Потом сделаю компонент проверки.

---
+++ client/src/utils/auth.js
@@ -0,0 +1,21 @@
+import Cookies from "js-cookie";
+import cookie from "cookie";
+
+export const isLoggedIn = (reqCookies = null) => {
+    if (!reqCookies) {
+        return !!Cookies.get("auth_token");
+    }
+
+    return !!cookie.parse(reqCookies).auth_token;
+}
+
+export const logIn = (token, role) => {
+    Cookies.set("auth_token", token, { expires: 24 / 24 });
+    localStorage.setItem("role", role);
+}
+
+export const logOut = () => {
+    Cookies.remove("auth_token");
+    localStorage.removeItem("role");
+    window.location.href = "/login";
+}
