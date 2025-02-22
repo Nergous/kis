231c29dec5ed1ed0a2accfc200ee9103d5f7a8a6
Attomex <e.primorov2@gmail.com>
2025-02-22 10:22:56 +0300

    Рефактор главной страницы

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Main from "../pages/Main/Main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "../pages/Main/layout/MainLayout";
 
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Main />} />
+                
+                <Route path="/" element={<MainLayout> <Main /> </MainLayout>} />
             </Routes>
         </BrowserRouter>
     );
